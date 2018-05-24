df4290d3baac7b49924ab0e5391d2d4c3240b805
Pontus Backman <ponbac@student.chalmers.se>
2018-05-24 12:08:31 +0200

    order ID

--- src/app/CheckoutDone.java
+++ src/app/CheckoutDone.java
@@ -1,11 +1,20 @@
 package app;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class CheckoutDone extends AnchorPane {
+
+    @FXML
+    Label orderID;
+
+    Random rand;
+
     public CheckoutDone() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/doneCheckOutElement.fxml"));
         fxmlLoader.setRoot(this);
@@ -17,5 +26,17 @@ public class CheckoutDone extends AnchorPane {
             throw new RuntimeException(exception);
         }
 
+        orderID.setText("Order-ID: " + getOrderID());
+    }
+
+    private String getOrderID() {
+        rand = new Random();
+        String id = "";
+
+        for (int i = 0; i < 6; i++) {
+            id += "" + rand.nextInt(10);
+        }
+
+        return id;
     }
 }
