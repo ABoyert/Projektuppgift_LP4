8bce6e5bfbac8869c4f5c2b8cfaa2d79d81dd0ce
Pontus Backman <ponbac@student.chalmers.se>
2018-05-24 12:15:22 +0200

    done page looks better

--- src/app/CheckoutDone.java
+++ src/app/CheckoutDone.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import sun.applet.Main;
 
 import java.io.IOException;
 import java.util.Random;
@@ -15,7 +16,9 @@ public class CheckoutDone extends AnchorPane {
 
     Random rand;
 
-    public CheckoutDone() {
+    MainController parentController;
+
+    public CheckoutDone(MainController parentController) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/doneCheckOutElement.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
@@ -25,6 +28,8 @@ public class CheckoutDone extends AnchorPane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+
+        this.parentController = parentController;
 
         orderID.setText("Order-ID: " + getOrderID());
     }
@@ -38,5 +43,10 @@ public class CheckoutDone extends AnchorPane {
         }
 
         return id;
+    }
+
+    @FXML
+    public void goToStart() {
+        parentController.shopButtonPressed();
     }
 }
